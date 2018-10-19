7d730e83af03b9fa9d66ddc3cb331fb201d91832
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-10-19 20:17:57 -0300

    error

--- js/controle/login.controller.js
+++ js/controle/login.controller.js
@@ -1,65 +1,30 @@
-var app = angular.module('app', ['ui.mask','angular-loading-bar']);
-
-app.controller('painelInicialController', function($scope, $http){
-    $scope.showCadastro = false;
-    $scope.noticia = objNoticia();
-    $scope.allNoticias = {};
+app.controller('loginController', function($scope, $http){
+   
+    $scope.login = {
+        usuario : "",
+        senha : ""
+    };
     
-    $scope.abreCadastroNoticia = function(){
-        $scope.showCadastro = true;
+    $scope.fazerLogin = function(){
+        
+        if($scope.login.usuario.trim()=="" || $scope.login.senha.trim()==""){
+            alert("Informe usuário e senha!");
+            return;
+        }
+        
+        $http
+            .post('../api/login', $scope.login)
+            .success(function(data){
+                console.log(data);
+            
+                if(data.logado){
+                    window.location = "painel-inicial.php"
+                } else {
+                    alert("Verifique usuário ou senha");   
+                }
+                
+            });
+        
     }
     
-    $scope.listarNoticias = function(){
-        $http.get('../api/listarNoticias')
-            .success(function(data){
-                $scope.allNoticias = data.noticias;
-            })
-            .error(function(){
-                alert("Falha em obter notícias");
-            });
-    };
-    
-    $scope.cadastrarNovaNoticia = function(){
-        $http
-            .post('../api/cadastrarNovaNoticia', $scope.noticia)
-            .success(function(data){
-                
-                if(!data.erro) {
-                    // deu certo o cadastro
-                    
-                    $.gritter.add({
-                        title : "Sucesso!",
-                        text : "Notícia cadastrada com sucesso!",
-                        class_name : "gritter"
-                    });
-                    
-                    $scope.showCadastro = false;
-                    $scope.noticia = objNoticia();
-                    $scope.listarNoticias();
-                } else {
-                    $.gritter.add({
-                        title : "Falha!",
-                        text : "Ocorreu um erro!",
-                        class_name : "gritter"
-                    }); 
-                }
-            
-            })
-            .error(function(){
-                alert("Falha geral da aplicação!");
-            });
-    };
-    
-    $scope.listarNoticias();
-    
 });
-
-function objNoticia(){
-    return {
-        idnoticia : -1,
-        noticiatitulo : "",
-        noticiadescricao : "",
-        noticiatexto : "",
-        noticiadata : ""
-    };
-}
